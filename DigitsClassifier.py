3dbf3eb5e4c6fc06ada7fde8173806ceeee494d9
ElPapi42 <whitman-2@hotmail.com>
2019-07-27 15:31:16 -0400

    Base Model Version

--- DigitsClassifier.py
+++ DigitsClassifier.py
@@ -11,6 +11,29 @@ class DigitsClassifier(tf.keras.Model):
     def __init__(self):
         super(DigitsClassifier, self).__init__(self)
 
+        self.conv_0 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", input_shape=[28, 28, 1])
+        self.pooling_0 = tf.keras.layers.MaxPool2D(2)
+        self.conv_1 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same")
+        self.pooling_1 = tf.keras.layers.MaxPool2D(4)
+
+        self.flatten = tf.keras.layers.Flatten()
+
+        self.hiden = tf.keras.layers.Dense(10, "relu")
+        self.softmax = tf.keras.layers.Softmax()
+
+    def call(self, inputs):
+
+        output = self.conv_0(inputs)
+        output = self.pooling_0(output)
+        output = self.conv_1(output)
+        output = self.pooling_1(output)
+
+        output = self.flatten(output)
+        output = self.hiden(output)
+        output = self.softmax(output)
+
+        return output
+
 
 
 
--- MnistClassifier.py
+++ MnistClassifier.py
@@ -1,17 +1,35 @@
 import tensorflow as tf
-import tensorflow.keras.layers as layers
+import tensorflow.keras.datasets.mnist as mn
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import random
+from DigitsClassifier import DigitsClassifier
 
-mnist = pd.read_csv('./Resources/train.csv')
+mnist_train, mnist_test = tf.keras.datasets.mnist.load_data("./Resources")
+mnist_digits = np.concatenate([mnist_train[0], mnist_test[0]], axis=0)
+mnist_labels = np.concatenate([mnist_train[1], mnist_test[1]], axis=0)
 
-labels = mnist[["label"]].values
-labels = np.squeeze(labels)
+labels = mnist_labels
 
-digits = mnist.drop(["label"], axis=1).values
-digits = np.reshape(digits, [np.shape(digits)[0], 28, 28])
+digits = mnist_digits
+digits = np.reshape(digits, [np.shape(digits)[0], 28, 28, 1])
 digits = digits/255
 
-dataset = tf.data.Dataset.from_tensor_slices((digits, labels)).batch(1)
+print(np.shape(digits))
+
+def get_sample():
+    for index in range(0, np.shape(labels)[0]):
+        yield (digits[index], labels[index])
+
+
+train_ds = tf.data.Dataset.from_generator(get_sample, (tf.float32, tf.int32))
+test_ds = train_ds.take(10000).batch(1000).prefetch(2)
+train_ds = train_ds.skip(10000).batch(1000).prefetch(2)
+
+model = DigitsClassifier()
+model.compile(tf.keras.optimizers.Adam(0.001), tf.keras.losses.SparseCategoricalCrossentropy())
+model.fit(train_ds, epochs=50, verbose=2, validation_data=test_ds)
+#for digit, label in dataset.take(10):
+    #model(digit)
