a459c9f3b549f1b8d647416236447caca28d6fa0
ElPapi42 <whitman-2@hotmail.com>
2019-07-26 23:36:19 -0400

    Labels and Digit Image extraction and preprocessing complete Added DigitClassifier Class

---
+++ DigitsClassifier.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+import tensorflow.keras.layers as layers
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class DigitsClassifier(tf.keras.Model):
+    """MNIST Digit Classifier"""
+
+    def __init__(self):
+        super(DigitsClassifier, self).__init__(self)
+
+
+
+
--- MnistClassifier.py
+++ MnistClassifier.py
@@ -1,6 +1,17 @@
 import tensorflow as tf
-#import tensorflow.keras.layers as layers
+import tensorflow.keras.layers as layers
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+mnist = pd.read_csv('./Resources/train.csv')
+
+labels = mnist[["label"]].values
+labels = np.squeeze(labels)
+
+digits = mnist.drop(["label"], axis=1).values
+digits = np.reshape(digits, [np.shape(digits)[0], 28, 28])
+digits = digits/255
+
+dataset = tf.data.Dataset.from_tensor_slices((digits, labels)).batch(1)
