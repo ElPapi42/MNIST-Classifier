9bfdb970c10f6c07b4339ecd97b3436590cbe2a9
ElPapi42 <whitman-2@hotmail.com>
2019-07-30 20:47:27 -0400

    Fixed Model Overfitting
    Added some pretrained weights

--- DigitsClassifier.py
+++ DigitsClassifier.py
@@ -5,31 +5,50 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+class ConvBlock(tf.keras.layers.Layer):
+    """Convolutional Block featuring Conv2D + Pooling"""
+
+    def __init__(self, conv_deep=1, kernels=32, kernel_size=3, pool_size=2, dropout_rate=1.0):
+        super(ConvBlock, self).__init__(self)
+
+        self.conv_layers = []
+        self.pooling_layers = []
+        self.bnorm_layers = []
+        self.dropout_layers = []
+        for index in range(0, conv_deep):
+            self.conv_layers.append(tf.keras.layers.Conv2D(filters=kernels, kernel_size=kernel_size, padding="same", activation="relu"))
+            self.pooling_layers.append(tf.keras.layers.MaxPool2D(pool_size=pool_size))
+            self.bnorm_layers.append(tf.keras.layers.BatchNormalization())
+            self.dropout_layers.append(tf.keras.layers.Dropout(dropout_rate))
+
+    def call(self, inputs, training=False):
+        output = inputs
+
+        for (conv, pooling, bnorm, dropout) in zip(self.conv_layers, self.pooling_layers, self.bnorm_layers, self.dropout_layers):
+            output = conv(output)
+            output = pooling(output)
+            output = bnorm(output)
+
+            if training:
+                output = dropout(output)
+
+        return output
+
 class DigitsClassifier(tf.keras.Model):
     """MNIST Digit Classifier"""
 
     def __init__(self):
         super(DigitsClassifier, self).__init__(self)
 
-        self.conv_0 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", input_shape=[28, 28, 1])
-        self.pooling_0 = tf.keras.layers.MaxPool2D(2)
-        self.conv_1 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same")
-        self.pooling_1 = tf.keras.layers.MaxPool2D(4)
+        self.conv = ConvBlock(conv_deep=1, kernels=2)
 
         self.flatten = tf.keras.layers.Flatten()
-
-        self.hiden = tf.keras.layers.Dense(10, "relu")
-        self.softmax = tf.keras.layers.Softmax()
+        self.softmax = tf.keras.layers.Dense(10, "softmax")
 
     def call(self, inputs):
 
-        output = self.conv_0(inputs)
-        output = self.pooling_0(output)
-        output = self.conv_1(output)
-        output = self.pooling_1(output)
-
+        output = self.conv(inputs)
         output = self.flatten(output)
-        output = self.hiden(output)
         output = self.softmax(output)
 
         return output
--- MnistClassifier.py
+++ MnistClassifier.py
@@ -1,36 +1,68 @@
 import tensorflow as tf
-import tensorflow.keras.datasets.mnist as mn
+import tensorflow
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import random
+from PIL import Image
+
 from DigitsClassifier import DigitsClassifier
 
-mnist_train, mnist_test = tf.keras.datasets.mnist.load_data("./Resources")
-mnist_digits = np.concatenate([mnist_train[0], mnist_test[0]], axis=0)
-mnist_labels = np.concatenate([mnist_train[1], mnist_test[1]], axis=0)
+#Load Train Data
+(train_digits, train_labels), (eval_digits, eval_labels) = tf.keras.datasets.mnist.load_data("./Resources")
+kaggle_digits = pd.read_csv("./Resources/test.csv").values
 
-labels = mnist_labels
+#Preprocess
+train_digits = np.reshape(train_digits, [np.shape(train_digits)[0], 28, 28, 1])/255.0
+eval_digits = np.reshape(eval_digits, [np.shape(eval_digits)[0], 28, 28, 1])/255.0
+kaggle_digits = np.reshape(kaggle_digits, [np.shape(kaggle_digits)[0], 28, 28, 1])/255.0
 
-digits = mnist_digits
-digits = np.reshape(digits, [np.shape(digits)[0], 28, 28, 1])
-digits = digits/255
+#Generator
+def get_sample(digits, return_labels=False, labels=None):
+    if(return_labels):
+        if(np.shape(digits)[0] == np.shape(labels)[0]):
+            for index in range(0, np.shape(digits)[0]):
+                yield (digits[index], labels[index])
+        else:
+            raise ValueError("Digits and Labels dont have the same numberof samples")
+    else:
+        for index in range(0, np.shape(digits)[0]):
+            yield (digits[index])
 
-print(np.shape(digits))
+def transform_sample(digit, label):
+    rot = random.randint(-1, 2)
+    t_digit = digit
+    t_digit = tf.compat.v2.image.rot90(t_digit, rot)
 
-def get_sample():
-    for index in range(0, np.shape(labels)[0]):
-        yield (digits[index], labels[index])
+    return t_digit, label
 
+#Define datasets
+train_ds = tf.data.Dataset.from_generator(get_sample, (tf.float32, tf.int32), args=[train_digits, True, train_labels]).map(transform_sample, 100).batch(1000).prefetch(2)
+eval_ds = tf.data.Dataset.from_generator(get_sample, (tf.float32, tf.int32), args=[eval_digits, True, eval_labels]).batch(1000).prefetch(2)
+kaggle_ds = tf.data.Dataset.from_generator(get_sample, (tf.float32), args=[kaggle_digits]).batch(1000).prefetch(2)
 
-train_ds = tf.data.Dataset.from_generator(get_sample, (tf.float32, tf.int32))
-test_ds = train_ds.take(10000).batch(1000).prefetch(2)
-train_ds = train_ds.skip(10000).batch(1000).prefetch(2)
+"""
+for digits, label in train_ds.take(1):
+    print(label)
+    sns.regplot(data=digits)
+    plt.show()
+"""
 
+#Define model and load weights (Pretrained on google colab notebook)
 model = DigitsClassifier()
-model.compile(tf.keras.optimizers.Adam(0.001), tf.keras.losses.SparseCategoricalCrossentropy())
-model.fit(train_ds, epochs=50, verbose=2, validation_data=test_ds)
+model.compile(tf.keras.optimizers.Adadelta(7.0), tf.keras.losses.SparseCategoricalCrossentropy(), [tf.keras.metrics.SparseCategoricalAccuracy()])
+model.train_on_batch(train_ds.take(1))
+model.load_weights("./Weights/06/weights")
+print(model.summary())
+#model.fit(train_ds, epochs=50, verbose=2, validation_data=eval_ds)
 
-for digit, label in test_ds.take(1):
-    print(tf.argmax(model(digit), axis=1), label)
+index = 1
+results = [[], []]
+for test_batch in kaggle_ds:
+    results[0] = tf.concat([results[0], range(index, index + 1000)], axis=0)
+    results[1] = tf.concat([results[1], tf.argmax(model(test_batch), axis=1)], axis=0)
+    index += 1000
+
+results_df = pd.DataFrame({"ImageId":results[0], "Label":results[1]}, dtype=np.int32)
+results_df.to_csv("./Results/06/results.csv", index=False) 
